c6a86ae6be9ddc027328302d8c80926e9e936795
DakotaWBaker <dakota.baker@student.awesomeinc.org>
2022-11-10 03:21:55 +0000

    saving

--- backend_bistro/menu/admin.py
+++ backend_bistro/menu/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Menu_Item, Category, Cuisine
 
+
 admin.site.register(Menu_Item)
 # admin.site.register(Category)
 # admin.site.register(Cuisine)
