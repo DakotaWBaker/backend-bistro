a4f5e897a649563678fb45d7327cf2385b1eabef
DakotaWBaker <dakota.baker@student.awesomeinc.org>
2022-11-11 02:57:57 +0000

    adding another feature

--- backend_bistro/menu/admin.py
+++ backend_bistro/menu/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Menu_Item, Category, Cuisine, Ingredient
+from .models import Menu_Item, Category, Cuisine, Ingredient, Location
 
 
 admin.site.register(Menu_Item)
 admin.site.register(Category)
 admin.site.register(Cuisine)
 admin.site.register(Ingredient)
+admin.site.register(Location)
--- backend_bistro/menu/models.py
+++ backend_bistro/menu/models.py
@@ -15,12 +15,17 @@ class Category(models.Model):
         return self.category_name
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 200, null = True, blank = True)
+    amount = models.CharField(max_length = 200, null = True, blank = True)
 
     
     def __str__(self):
         return self.name
-        
+class Location(models.Model):
+    location = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.location
 
 class Menu_Item(models.Model):
     title = models.CharField(max_length = 200, null = True, name = 'title')
@@ -29,27 +34,25 @@ class Menu_Item(models.Model):
     cuisine = models.ForeignKey("Cuisine", on_delete=models.CASCADE)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     ingredients = models.ManyToManyField(Ingredient)
-
+    location = models.ManyToManyField(Location)
+    
     def json(self):
+        location = [i.id for i in Menu_Item.objects.get(pk=self.id).location.all()]
         return {
         'title': self.title,
         'description': self.description,
         'price': self.price,
+        'location': location,
         'cuisine': {
             'title': self.cuisine.cuisine_name,
         },
         'category': { 
             'title': self.category.category_name },
-        # 'ingredients': [ {"name": i.name, "amt": '10 cups' } for i in Menu_Item.objects.get(pk=self.id).ingredients.all() ],
-        'ingredients':  [{"name": i.name, "amt": '10 cups' } for i in Menu_Item.objects.get(pk=self.id).ingredients.all() ],
+        'ingredients':  [{"name": i.name, "amount": i.amount, } for i in Menu_Item.objects.get(pk=self.id).ingredients.all() ],
         }
 
 
     def __str__(self):
         return self.description
 
-# class ModelIngredient(models.Model):
-#     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-#     menu_item = models.ForeignKey(Menu_Item, on_delete=models.CASCADE)
-#     name = models.CharField(max_length = 200)
     
